324e618ac69f809b4df281f0868cf2dcd3344bd6
ChitwanRana <kushr4446@gmail.com>
2024-10-30 12:19:01 +0530

    Add static files

--- myStockProject/settings.py
+++ myStockProject/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Path to the directory where static files will be collected
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ # Path to the directory where static files will be collected
 
 
 MEDIA_URL = '/media/'
